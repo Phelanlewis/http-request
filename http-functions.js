eaa64692d407eec45ffab60c3597624cbcbbf7f8
Phelan Lewis <phelanlewis@gmail.com>
2017-01-16 21:19:05 +0000

    Fifth one completed

---
+++ http-functions.js
@@ -0,0 +1,19 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback){
+
+  https.get(options, function (response) {
+
+  response.setEncoding('utf8');
+
+  var bufferedData = '';
+
+  response.on('data', function (data) {
+      bufferedData += data;
+  });
+
+  response.on('end', function() {
+    callback(bufferedData);
+      });
+    });
+  };
